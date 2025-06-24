197dcca08fc8183a60e74eeea01ac92b31a11980
Junyou Park <pjy008008@gmail.com>
2025-06-24 12:07:25 +0900

    Initial commit

---
+++ src/test/java/com/example/cloud_back/CloudBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cloud_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
